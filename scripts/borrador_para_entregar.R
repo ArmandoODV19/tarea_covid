ce0e1f02fe4adcb48752f69ecd35b8514c0b20dc
Armando Ocampo <fooseaov@gmail.com>
2023-01-05 17:46:00 -0600

    agregando comparativa de casos por millon vs muertes por millon de habitantes

--- scripts/borrador_para_entregar.R
+++ scripts/borrador_para_entregar.R
@@ -43,5 +43,40 @@ ggplot(covid_limpio, aes(x = total_cases_per_million, y = gdp_per_capita,
   xlab("Total de casos por millon de habitantes") +
   ylab("Ingreso bruto por habitante") +
   ggtitle('Total de casos por millon de habitantes e ingreso bruto por habitante') +
-  theme(plot.title = element_text(hjust = 1)) +
-  theme_bw()
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+# obtener media, mediana, sd de casos por covid por millon de habitantes
+
+ggplot(covid_limpio, aes(x = total_cases_per_million)) +
+  geom_histogram(bins = 15)
+
+mean(covid_limpio$total_cases_per_million, na.rm = TRUE)
+sd(covid_limpio$total_cases_per_million, na.rm = TRUE)
+median(covid_limpio$total_cases_per_million, na.rm = TRUE)
+
+########################################
+
+# evaluar la relacion de muertes por millon y los casos por millon
+
+ggplot(covid_limpio, aes(x = total_cases_per_million, y = total_deaths_per_million,
+                         color = continent)) +
+  geom_point(size = 7, alpha = 0.6)+
+  geom_smooth(method = 'lm', na.rm = TRUE, fullrange= TRUE,
+              aes(group=1),colour="dodgerblue")+
+  xlab("Casos totales por millón") +
+  ylab("Muertes totales por millón") +
+  ggtitle("Casos por millón y muertes por millón") +
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# obtener media, mediana, sd de muertes por covid por millon de habitantes
+
+ggplot(covid_limpio, aes(x = total_deaths_per_million)) +
+  geom_histogram(bins = 15)
+
+mean(covid_limpio$total_deaths_per_million, na.rm = TRUE)
+sd(covid_limpio$total_deaths_per_million, na.rm = TRUE)
+median(covid_limpio$total_deaths_per_million, na.rm = TRUE)
+
+
