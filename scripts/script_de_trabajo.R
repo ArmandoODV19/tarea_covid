ae436ef321eb49973f7e164d87bce607b11508f7
Armando Ocampo <fooseaov@gmail.com>
2023-01-05 15:09:14 -0600

    script de trabajo

--- scripts/script_de_trabajo.R
+++ scripts/script_de_trabajo.R
@@ -1,1 +1,18 @@
 # script de trabajo
+
+# llamando archivo de trabajo COVID-19
+
+covid <- read_csv('raw_data/owid-covid-data.csv')
+
+# convirtiendo archivo a .rds para eficazar el trabajo
+
+saveRDS(covid, "clean_data/covid_dataset.rds")
+
+# llamando archivo de trabajo covid_dataset.rds
+
+covid <- readRDS("clean_data/covid_dataset.rds")
+
+# visualizar dataset
+
+glimpse(covid)
+summary(covid)
