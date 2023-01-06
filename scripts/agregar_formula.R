2a7525502a0fb194462735224102dabb3acd1566
Armando Ocampo <fooseaov@gmail.com>
2023-01-06 11:56:22 -0600

    modificando plots

--- scripts/agregar_formula.R
+++ scripts/agregar_formula.R
@@ -1,16 +1,3 @@
-eq <- function(x,y) {
-  m <- lm(y ~ x)
-  as.character(
-    as.expression(
-      substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
-                 list(a = format(coef(m)[1], digits = 4),
-                      b = format(coef(m)[2], digits = 4),
-                      r2 = format(summary(m)$r.squared, digits = 3)))
-    )
-  )
-}
-
-
 ggplot(covid_limpio, aes(x = total_cases_per_million, y = total_deaths_per_million,
                          color = continent)) +
   geom_point(size = 7, alpha = 0.7) +
@@ -23,27 +10,8 @@ ggplot(covid_limpio, aes(x = total_cases_per_million, y = total_deaths_per_milli
   ggtitle("Casos por millón y muertes por millón") +
   labs(caption = "my caption",
        color = 'Continente') +
-  theme_bw()+
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.caption = element_text(hjust = 0)) +
-  geom_text(x = 2, y = 300, label = eq(covid_limpio$total_cases_per_million,
-                                       covid_limpio$total_deaths_per_million),
-            parse = TRUE)
-
-### segundo intento
-
-ggplot(covid_limpio, aes(x = total_cases_per_million, y = total_deaths_per_million,
-                         color = continent)) +
-  geom_point(size = 7, alpha = 0.7) +
-  scale_color_manual(values = c('dodgerblue', 'forestgreen', 'salmon',
-                                'wheat', 'lightsteelblue', 'chocolate')) +
-  geom_smooth(method = 'lm', na.rm = TRUE, fullrange= TRUE,
-              aes(group=1),colour="azure4")+
-  xlab("Casos totales por millón") +
-  ylab("Muertes totales por millón") +
-  ggtitle("Casos por millón y muertes por millón") +
-  labs(caption = "my caption",
-       color = 'Continente') +
+  scale_x_continuous(labels = label_comma()) +
+  scale_y_continuous(labels = label_comma()) +
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
         plot.caption = element_text(hjust = 0)) +
